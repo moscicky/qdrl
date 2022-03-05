9335f7fc343c212a1d748c0ea10ca1c41c5aa1b5
maciej <moscicki.maciej619@gmai.com>
2022-03-05 13:49:01 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,30 @@
+.PHONY: clean clean-test clean-pyc clean-build
+
+clean: clean-build clean-pyc clean-test
+
+clean-build:
+	rm -rf .mypy_cache/
+	rm -rf test-reports/
+	rm -rf build/
+	rm -rf dist/
+	rm -rf .eggs/
+	find . -name '*.egg-info' -exec rm -rf {} +
+	find . -name '*.egg' -exec rm -rf {} +
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -rf {} +
+	find . -name '*.pyo' -exec rm -rf {} +
+	find . -name '*~' -exec rm -rf {} +
+	find . -name '__pycache__' -exec rm -rf {} +
+
+clean-test:
+	rm -rf .pytest_cache
+
+
+clean: clean-build
+
+install-dev: clean ## install dev packages
+	pip install -r requirements_dev.txt
+
+lint:
+	flake8 qdrl tests
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+
+install_requirements = [
+    "torch==1.9.0"
+    "numpy==1.21.2",
+    "pyspark==2.4.8",
+    "pyspark-stubs==2.4.0"
+    "pandas==1.0.5"
+]
+
+test_requirements = [
+    "flake8",
+    "pytest==6.2.4",
+    "tox==3.24.5",
+    "mypy==0.931"
+]
+
+setup(
+    name="qdrl",
+    author="moscicky",
+    description="Library for query-document representation learning",
+    packages=[package for package in find_packages(exclude=["tests*"])],
+    version="0.1.0",
+    license="MIT",
+    install_requires=install_requirements,
+    extras_require=dict(test=test_requirements),
+    test_suites="tests"
+)
